70c8f5f7b87021cb88bb0901a473e523c0fe381c
Cameron Knight <cameron.knight@moblico.com>
2016-08-12 15:36:36 -0500

    Remove custom deserialization.

--- MoblicoSDK/Entities/MLCProduct.m
+++ MoblicoSDK/Entities/MLCProduct.m
@@ -10,39 +10,4 @@
 
 @implementation MLCProduct
 
-void _MLCConvertDateForKey(NSMutableDictionary *json, NSString *key) {
-    NSString *dateString = json[key];
-	if (dateString == nil || [dateString isKindOfClass:NSNull.class]) {
-		return;
-	}
-    static NSDateFormatter *dateFormatter;
-    if (!dateFormatter) {
-        dateFormatter = [[NSDateFormatter alloc] init];
-        dateFormatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
-        dateFormatter.lenient = YES;
-        dateFormatter.dateStyle = NSDateFormatterMediumStyle;
-        dateFormatter.timeStyle = NSDateFormatterMediumStyle;
-    }
-    NSLog(@"dateFormat: %@", [dateFormatter stringFromDate:[NSDate date]]);
-    NSLog(@"dateString: %@", dateString);
-    NSDate *date = [dateFormatter dateFromString:dateString];
-    NSLog(@"date: %@", date);
-    NSTimeInterval timeInterval = date.timeIntervalSince1970;
-    NSNumber *timeStamp = @(timeInterval * 1000.0);
-    NSLog(@"timeStamp: %@", timeStamp);
-
-    json[key] = @(timeStamp.longLongValue);
-}
-
-- (instancetype)initWithJSONObject:(NSDictionary *)jsonObject {
-    NSMutableDictionary *json = [jsonObject mutableCopy];
-    _MLCConvertDateForKey(json, @"expirationDate");
-    _MLCConvertDateForKey(json, @"introDate");
-    _MLCConvertDateForKey(json, @"revDate");
-
-    self = [super initWithJSONObject:json];
-
-    return self;
-}
-
 @end
