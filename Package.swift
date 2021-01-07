905db79042c453b0ad3888a0e1398e040a3a4a79
Cameron Knight <cameron.knight@moblico.com>
2021-01-07 15:51:42 -0600

    Fix type

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(name: "MoblicoSDK",
                       platforms: [.macOS(.v10_13),
-                                  .iOS(.v12)],
+                                  .iOS(.v12),
                                   .watchOS(.v4),
                                   .tvOS(.v11)],
                       products: [.library(name: "MoblicoSDK",
